9f16a58c8f4c39d3a16aff3bb325be5a24996979
Duyen Nguyen <hongduyen0705@gmail.com>
2022-09-05 09:38:58 +0300

    basic functions complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 
 // Task, responsible for rendering a single task
 const Task = ({ task, handleClick, handleDelete, handleVote }) => {
   return (
     <li>
       {task.content}
-      <p>Votes: {task.votes}</p>
+
       <button onClick={handleClick}>
         {task.complete ? "completed" : "not completed"}
       </button>
-      <button onClick={handleDelete}>delete</button>
+
+      <span>Votes: {task.votes}</span>
       <button onClick={handleVote}>Vote</button>
+      <button onClick={handleDelete}>delete</button>
     </li>
   );
 };
